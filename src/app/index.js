fe045d22e8c7033510527f72fd71f259ee0c40fd
gdi2290 <github@gdi2290.com>
2015-05-21 10:25:59 -0700

    chore: wip debug

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,20 @@
-import {bootstrap, Component, View} from 'angular2';
+import {BrowserDomAdapter} from 'angular2/src/dom/browser_adapter';
+BrowserDomAdapter.makeCurrent();
+import {bootstrap, Component, View} from 'angular2/angular2';
 
 
-@Component({selector: 'my-app'})
-@View({template: `<h1>Hello</h1>`})
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: `<h1>Hello</h1>`
+})
 class App {
   constructor() {
+    console.log('wat');
   }
 }
 
-bootstrap(App);
+export function main() {
+  return bootstrap(App);
+};
